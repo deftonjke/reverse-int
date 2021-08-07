610e1bcfeecd681dae1f00a0a1dfa6378542e34b
deftonjke <defton@mail.ru>
2021-08-07 16:56:57 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  return (n < 0) ? +String(n).slice(1)
+                             .split('')
+                             .reverse()
+                             .join('') : 
+                   +String(n).split('')
+                             .reverse()
+                             .join('');
 }
